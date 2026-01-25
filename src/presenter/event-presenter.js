fb609745fcfd768378c1e97e93d27f4a42783114
Echenko <ehenko.v.g@yandex.ru>
2026-01-25 21:33:44 +0300

    fix: rename

--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -2,7 +2,7 @@ import EventSortView from '../view/event-sort-view.js';
 import EventListView from '../view/event-list-view.js';
 import EventPointView from '../view/event-point-view.js';
 import EventPointEditView from '../view/event-point-edit-view.js';
-import EventPointAddView from '../view/event-point-add.js';
+import EventPointAddView from '../view/event-point-add-view.js';
 
 import {render} from '../render.js';
 
